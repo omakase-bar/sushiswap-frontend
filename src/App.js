20bf4bc214491c699b64736cfd09e69174ddd1f7
omakasebar <omakasebar@gmail.com>
2020-11-13 22:57:20 +0100

    remove notice, hotfix

--- src/App.js
+++ src/App.js
@@ -108,8 +108,8 @@ import SwapWrapper from "./pages/Swap";
 import PoolWrapper from "./pages/Pool";
 
 import ModalsProvider from "./shared/contexts/ModalsContext";
-import NoticeModal from "./components/Modals/NoticeStandalone";
-import useModalOpen from "./shared/hooks/useModalOpen";
+// import NoticeModal from "./components/Modals/NoticeStandalone";
+// import useModalOpen from "./shared/hooks/useModalOpen";
 
 const App = () => {
   const globalData = useGlobalData();
@@ -124,7 +124,7 @@ const App = () => {
       globalChartData &&
       Object.keys(globalChartData).length > 0 ? (
         <>
-          // <NoticeModal isOpen={notice.isOpen} closeModal={notice.hide} />
+          {/* <NoticeModal isOpen={notice.isOpen} closeModal={notice.hide} /> */}
           <Router>
             <Web3ReactManager>
               <Switch>
